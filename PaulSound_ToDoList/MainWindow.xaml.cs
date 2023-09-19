4a4e27ab3f4e9477e2b00304f0b98bc922770122
PaulSound <pavelsound94@gmail.com>
2023-09-19 15:40:32 +0300

    Binded coleection and put ItemAdded Ivent

--- PaulSound_ToDoList/MainWindow.xaml.cs
+++ PaulSound_ToDoList/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using PaulSound_ToDoList.MODEL;
-
+using System.ComponentModel;
 namespace PaulSound_ToDoList
 {
     /// <summary>
@@ -21,10 +21,28 @@ namespace PaulSound_ToDoList
     /// </summary>
     public partial class MainWindow : Window
     {
+        BindingList<DataModel> dataJob=new BindingList<DataModel>()
+        { 
+          new DataModel(){ Job="Wash hair",Done=false,Desc="You should wash your hair"},
+          new DataModel(){Job="Wipe your ass",Done=true,Desc="Ass should be wiped"}
+        
+        };
         public MainWindow()
         {
             
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+      
+            dataGridToDo.ItemsSource = dataJob; // привязал коллекцию к свойству ItemSource именнованной сетке DateGrid(dataGridToDo)
+            dataJob.ListChanged += AddNewJob;
+        }
+
+        private void AddNewJob(object sender, ListChangedEventArgs e)
+        {
+            MessageBox.Show(e.ListChangedType.ToString());
         }
     }
 }
