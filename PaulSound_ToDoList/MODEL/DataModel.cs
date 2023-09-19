d872121ba06269a4b31c31a628694d8e536e1ca7
PaulSound <pavelsound94@gmail.com>
2023-09-19 14:06:31 +0300

    Binded columns to DataModel

---
+++ PaulSound_ToDoList/MODEL/DataModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaulSound_ToDoList.MODEL
+{
+    public class DataModel // Все свойсва прикрепляются через свйоства  Binding ="{inding Path = }"
+    {
+        private string _job;
+        private bool _done;
+        private string _desc;
+
+        public string Job { get=>_job; set=>_job=value; }
+        public string Desc { get=>_desc; set=>_desc=value; }
+        public bool Done { get=>_done; set=>_done=value; }
+    }
+}
--- PaulSound_ToDoList/MainWindow.xaml.cs
+++ PaulSound_ToDoList/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PaulSound_ToDoList.MODEL;
+
 namespace PaulSound_ToDoList
 {
     /// <summary>
